1e934279621a8b6c91f3447cf4b87afc11dd8553
thigas420 <thiago420sc@gmail.com>
2025-05-14 22:17:14 -0300

    adicionando sistema de toasts

--- Front/src/pages/Login/index.tsx
+++ Front/src/pages/Login/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Container, Form } from "./styles";
 import { useAuth } from "../../context/AuthProvider";
 import { useNavigate } from "react-router";
+import { Bounce, toast, ToastContainer } from "react-toastify";
 
 const Register = () => {
   const { login } = useAuth();
@@ -10,24 +11,31 @@ const Register = () => {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  
+  const notify = () => toast('Wow so easy !');
 
   const handleLogin = async () => {
     const response = await login(email, password);
 
+    notify();
+
     if (response) {
-      console.log('Login successful');
+      toast.success('Logado com sucesso!', {
+        position: 'top-right',
+      });
       navigate('/user');
     } else {
-      console.log('Login failed');
+      notify();
     }
   }
 
   return (
     <>
       <Container>
+        <ToastContainer />
         <Form>
-          <button 
-            type="button" 
+          <button
+            type="button"
             onClick={() => {
               setEmail('teste@gmail.com');
               setPassword('12345678');
@@ -37,6 +45,8 @@ const Register = () => {
           <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
           <input type="text" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
           <button type="button" onClick={handleLogin}>Login</button>
+          <ToastContainer />
+          <button type="button" onClick={notify}>Notify !</button>
         </Form>
       </Container>
     </>
