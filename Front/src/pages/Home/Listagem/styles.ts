e9e2a00e28f259d31ee5a6c5caf61e115444e0e8
thigas420 <thiago420sc@gmail.com>
2025-05-21 22:27:19 -0300

    mucha coisa e mudando para casa

--- Front/src/pages/Home/Listagem/styles.ts
+++ Front/src/pages/Home/Listagem/styles.ts
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  height: 100%;
   width: 100%;
   display: flex;
   flex-direction: column;
@@ -54,19 +53,88 @@ export const Listagem = styled.div`
     margin-top: 20px;
 
     .card {
+      position: relative;
       display: flex;
       flex-direction: column;
       cursor: pointer;
+      gap: 15px;
+      margin-bottom: 35px;
 
-      img {
-        width: 15vw;
+      .labelFechado {
+        right: 10px;
+        top: 10px;
+        position: absolute;
+        display: none;
+        padding: 5px 8px;
+        font-family: 'Inter', sans-serif;
+        font-size: 0.9rem;
+        color: #ffffff;
+        background-color:rgba(0, 0, 0, 0.5);
+        border-radius: 5px;
+        z-index: 1;
       }
 
-      .cardTitle {
-        font-family: 'Inter', sans-serif;
-        font-size: 1.5rem;
-        color: #9E6C00;
-        font-weight: 600;
+      .fotoFornecedor {
+        width: 15vw;
+        z-index: 0;
+      }
+
+      .informacoes {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+
+        .nome {
+          font-family: 'Inter', sans-serif;
+          font-size: 1.4rem;
+          color: #000000;
+          font-weight: 400;
+        }
+        
+        .tempoDistancia {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          align-items: center;
+
+          .tempo {
+            font-family: 'Inter', sans-serif;
+            font-size: 1rem;
+            color: #9F9F9F;
+            font-weight: 700;
+          }
+
+          .distancia {
+            padding: 5px 8px;
+            font-family: 'Inter', sans-serif;
+            font-size: 0.9rem;
+            color: #ffffff;
+            background-color: #C9A862;
+            border-radius: 5px;
+            font-weight: 400;
+          }
+        }
+
+        .estrelas {
+          display: flex;
+          flex-direction: row;
+          gap: 5px;
+
+          img {
+            width: 20px;
+            height: 20px;
+          }
+        }
+      }
+    }
+
+    .card.fechado {
+      .labelFechado {
+        display: flex;
+      }
+
+      .fotoFornecedor {
+        filter: grayscale(100%);
       }
     }
   }
